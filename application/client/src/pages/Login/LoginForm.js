fcdbef17a73123a0255fcf3fb7c6ef6d80c504b8
banting <bantingyeelin@gmail.com>
2023-10-23 11:27:57 -0700

    feat-setting up login pages

--- application/client/src/pages/Login/LoginForm.js
+++ application/client/src/pages/Login/LoginForm.js
@@ -3,7 +3,11 @@ import React from "react";
 const LoginForm = () => {
     return{
         <div className-"cover">
-        
+        <h1>Login</h1>
+        <input type= "text" placeholder="username" /> 
+        <input type= "password" placeholder="password" /> 
+
+        <div>
         
         </div>
     }
