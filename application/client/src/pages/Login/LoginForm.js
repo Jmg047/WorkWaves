495e2c7787652be5b06360258405c91939ad339e
banting <bantingyeelin@gmail.com>
2023-10-23 11:22:11 -0700

    feat-creating login and home pages

--- application/client/src/pages/Login/LoginForm.js
+++ application/client/src/pages/Login/LoginForm.js
@@ -1,1 +1,12 @@
-// yes
+import React from "react";
+
+const LoginForm = () => {
+    return{
+        <div className-"cover">
+        
+        
+        </div>
+    }
+}
+
+export default LoginForm
