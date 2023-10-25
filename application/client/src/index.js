f6d4eeec64e991599b64382d11acef3f591275c4
Jmg047 <90755835+Jmg047@users.noreply.github.com>
2023-10-25 12:04:59 -0700

    fix(index.js, component/ui/CategorySideBarData.js): eslint fix.

--- application/client/src/index.js
+++ application/client/src/index.js
@@ -4,7 +4,7 @@ import App from './App'
 
 // * IMPORT EVERY STYLE.CSS HERE
 import './component/ui/Navbar.css'
-import './component/ui/CategorySideBar.css'
+// import './component/ui/CategorySideBar.css'
 import './component/ui/App.css'
 
 const domNode = document.getElementById('root')
