f3cbce2dbe26071ff99fe130e79b7ecaa760ed45
Jmg047 <90755835+Jmg047@users.noreply.github.com>
2023-10-25 15:36:16 -0700

    fix(index.js): css file uncommentended.

--- application/client/src/index.js
+++ application/client/src/index.js
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 
 // * IMPORT EVERY STYLE.CSS HERE
-// import './component/ui/Navbar.css'
-// import './component/ui/CategorySideBar.css'
+import './component/ui/Navbar.css'
+import './component/ui/CategorySideBar.css'
 import './component/ui/App.css'
 
 const domNode = document.getElementById('root')
