93da140fa9d30e8899048199c26c044ed4c7c172
Sidney <sid.14boys@yahoo.fr>
2023-10-26 03:25:00 -0700

    Homepage Basic Implementation done

--- application/client/src/component/ui/Feed.js
+++ application/client/src/component/ui/Feed.js
@@ -1,14 +1,46 @@
 import React from 'react'
 import FeedCSS from './Feed.module.css'
+import bartenderJob from './bartenderJob.png'
 
 function Feed () {
   return (
     <div className={FeedCSS.feed}> Feed Feed
-        <div className={FeedCSS.grid}>Feed Grid
-            <div className='grid-item grid-item-1'></div>
-            <div className='grid-item grid-item-2'></div>
-            <div className='grid-item grid-item-3'></div>
-            <div className='grid-item grid-item-4'></div>
+        <div className={FeedCSS.post}>
+            <div className={`${FeedCSS.item} item-1`}>
+                <img src={bartenderJob} alt='Job' width='250' height='150' />
+                  Looking for a Bartender
+                  <div>
+                    <a href='/JobDetails'>Details</a>
+                    <a href='/JobRequested'>Send job request</a>
+                  </div>
+            </div>
+              
+            <div className={`${FeedCSS.item} item-2`}>
+              <img src={bartenderJob} alt='Job' width='250' height='150' />
+                Looking for a Bartender
+                <div>
+                  <a href='/JobDetails'>Details</a>
+                  <a href='/JobRequested'>Send job request</a>
+                </div>
+            </div>
+
+            <div className={`${FeedCSS.item} item-3`}>
+              <img src={bartenderJob} alt='Job' width='250' height='150' />
+                Looking for a Bartender
+                <div>
+                  <a href='/JobDetails'>Details</a>
+                  <a href='/JobRequested'>Send job request</a>
+                </div>
+            </div>
+            
+            <div className={`${FeedCSS.item} item-3`}>
+              <img src={bartenderJob} alt='Job' width='250' height='150' />
+                Looking for a Bartender
+                <div>
+                  <a href='/JobDetails'>Details</a>
+                  <a href='/JobRequested'>Send job request</a>
+                </div>
+            </div>
         </div>
     </div>
   )
