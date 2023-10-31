8065bce264cb4f6a2edd6e514649df182a867e39
AbdarrahmanAyyaz <abdarrahman19@yahoo.com>
2023-10-30 18:47:34 -0700

    feat: started work on API endpoints for login and registration

--- application/backend/api/api-server.js
+++ application/backend/api/api-server.js
@@ -115,7 +115,80 @@ app.get('/workers', async (req, res) => {
       res.status(500).json({ error: 'error accessing mongoDB data' })
     }
   })
-
+  app.post('/registration', async (req, res) => {
+    try {
+      const client = new MongoClient(mongoURI)
+      await client.connect()
+      
+      const db = client.db('demo')
+      const collection = db.collection('demo')
+      
+      const { username, password, email } = req.body // Use req.body to access POST data
+      
+      // Check if the username already exists in the database
+      const existingUser = await collection.findOne({ username: req.body.username })
+  
+      if (existingUser) {
+        client.close()
+        return res.status(400).json({ error: 'Username already exists' })
+      }
+  
+      // If the username doesn't exist, insert the new user into the database
+      const newUser = {
+        username,
+        password, // Note: You should hash and salt the password before storing it in a production environment.
+        email,
+        Type: 'Worker' // Assuming all registered users are of type 'Worker', you can adjust this as needed.
+      }
+  
+      const result = await collection.insertOne(newUser)
+  
+      client.close()
+      
+      res.status(201).json({ message: 'User registered successfully', insertedId: result.insertedId })
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ error: 'Error registering user' })
+    }
+  })
+  app.post('/login', async (req, res) => {
+    try {
+      const client = new MongoClient(mongoURI)
+      await client.connect()
+  
+      const db = client.db('demo')
+      const collection = db.collection('demo')
+  
+      const { username, password } = req.body // Use req.body to access POST data
+  
+      // Find the user with the provided username
+      const user = await collection.findOne({ username: req.body.username })
+      //const workers = await collection.find({ Type: 'Worker' }).toArray()
+  
+      if (!user) {
+        client.close()
+        return res.status(401).json({ error: 'User not found' })
+      }
+  
+      // For simplicity, we compare the password as plain text. In a real-world app, you should hash and compare it securely.
+      if (user.password !== password) {
+        client.close()
+        return res.status(401).json({ error: 'Invalid password' })
+      }
+  
+      // If the username and password match, you can consider the user authenticated.
+      // You can generate and send a JWT (JSON Web Token) to the client for further authentication.
+  
+      client.close()
+  
+      res.status(200).json({ message: 'Login successful', user })
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ error: 'Error during login' })
+    }
+  })
+  
+  
   app.listen(port, () => {
     console.log(`API server is running on port ${port}`)
   })
