14f1ab321e29ec0b650c9c7eb94c83b987828738
Sidney <sid.14boys@yahoo.fr>
2023-10-31 03:38:44 -0700

    comment out deleted

--- application/client/src/pages/Home/Home.js
+++ application/client/src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import React from 'react'
 // * COMPONENTS IMPORT
 // import CategorySideBar from 'ui/CategorySideBar'
 // import Feed from 'ui/Feed'
+
 import CategorySideBar from '../../component/ui/CategorySideBar'
 import Feed from '../../component/ui/Feed'
 
@@ -21,61 +22,3 @@ Home () {
 }
 
 export default Home
-
-// const GigList = ({ query }) => {
-//   const [data, setData] = useState([])
-//   const [dataType, setDataType] = useState('gigs') // Default to gigs
-
-//   useEffect(() => {
-//     // Determine the data type and fetch data accordingly
-//     const apiUrl = dataType === 'gigs' ? 'http://localhost:2000/get-gigs' : 'http://localhost:2000/get-workers'
-
-//     const requestOptions = {
-//       method: 'GET',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       }
-//     }
-
-//     fetch(apiUrl, requestOptions)
-//       .then(response => response.json())
-//       .then(data => {
-//         setData(data)
-//       })
-//       .catch(error => console.error('Error fetching data:', error))
-//   }, [query, dataType])
-
-//   useEffect(() => {
-//     // Update the data type when the query changes
-//     if (query === 'worker') {
-//       setDataType('workers')
-//     } else {
-//       setDataType('gigs')
-//     }
-//   }, [query])
-
-//   return (
-//     <div>
-//       {data.map((item, index) => (
-//         <div key={index}>
-//           {dataType === 'gigs' && (
-//             <div>
-//               {/* Render gig information here */}
-//               <p>{item.title}</p>
-//               <p>{item.description}</p>
-//             </div>
-//           )}
-//           {dataType === 'workers' && (
-//             <div>
-//               {/* Render worker information here */}
-//               <p>{item.FirstName} {item.LastName}</p>
-//               <p>{item.Location}</p>
-//             </div>
-//           )}
-//         </div>
-//       ))}
-//     </div>
-//   )
-// }
-
-// export default GigList
