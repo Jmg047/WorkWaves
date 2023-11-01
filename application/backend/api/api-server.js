57d3f4b10997a4396e7ff66a6fd4605d67aac7e3
Jmg047 <90755835+Jmg047@users.noreply.github.com>
2023-10-31 17:36:11 -0700

    fix: updated api logic for registeration and login.

--- application/backend/api/api-server.js
+++ application/backend/api/api-server.js
@@ -4,191 +4,192 @@ const express = require('express')
 const app = express()
 const port = 2000
 
-app.use(cors()) 
+app.use(cors())
 app.use(express.json()) // JSON parser
 
 const mongoURI = 'mongodb+srv://client_00:T5StQOdhg2QjJ4KV@cluster0.hhxszoa.mongodb.net/demo'
 
 app.get('/workers', async (req, res) => {
-    try {
-      const client = new MongoClient(mongoURI)
-      await client.connect()
-  
-      const db = client.db('demo')
-      const collection = db.collection('demo')
-  
-      const workers = await collection.find({ Type: 'Worker' }).toArray()
+  try {
+    const client = new MongoClient(mongoURI)
+    await client.connect()
 
-      client.close()
-      res.json(workers)
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ error: 'error accessing mongoDB data' })
-    }
-  })
+    const db = client.db('demo')
+    const collection = db.collection('demo')
 
-  app.get('/get-workers/', async (req, res) => {
-    try {
-      const client = new MongoClient(mongoURI)
-      await client.connect()
-  
-      const db = client.db('demo')
-      const collection = db.collection('demo')
-  
-      const firstName = req.query.FirstName
-      const lastName = req.query.LastName
-      const location = req.query.Location
-  
-      const query = {
-        Type: 'Worker',
-      }
-  
-      if (req.query.FirstName) {
-        query.FirstName = { $regex: new RegExp(req.query.FirstName, 'i') }
-      }
-  
-      if (req.query.LastName) {
-        query.LastName = { $regex: new RegExp(req.query.LastName, 'i') }
-      }
-  
-      if (req.query.Location) {
-        query.Location = { $regex: new RegExp(req.query.Location, 'i') }
-      }
-  
-      const workers = await collection.find(query).toArray()
-  
-      client.close()
-      res.json(workers)
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ error: 'error accessing mongoDB data' })
-    }
-  })
-  
-  app.get('/gigs', async (req, res) => {
-    try {
-      const client = new MongoClient(mongoURI)
-      await client.connect()
-  
-      const db = client.db('demo')
-      const collection = db.collection('demo')
-  
-      const gigs = await collection.find({ Type: { $ne: 'Worker' } }).toArray()
-  
-      client.close()
-      res.json(gigs)
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ error: 'error accessing mongoDB data' })
-    }
-  })
+    const workers = await collection.find({ Type: 'Worker' }).toArray()
 
-  app.get('/get-gigs/', async (req, res) => {
-    try {
-      const client = new MongoClient(mongoURI)
-      await client.connect()
-  
-      const db = client.db('demo')
-      const collection = db.collection('demo')
-  
-      const title = req.query.title
-      const location = req.query.location
-  
-      const query = {
-        Type: { $ne: 'Worker' },
-      }
-  
-      if (req.query.title) {
-        query.title = { $regex: new RegExp(req.query.title, 'i') }
-      }
-  
-      if (req.query.location) {
-        query.location = { $regex: new RegExp(req.query.location, 'i') }
-      }
-  
-      const gigs = await collection.find(query).toArray()
-  
-      client.close()
-      res.json(gigs)
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ error: 'error accessing mongoDB data' })
+    client.close()
+    res.json(workers)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'error accessing mongoDB data' })
+  }
+})
+
+app.get('/get-workers/', async (req, res) => {
+  try {
+    const client = new MongoClient(mongoURI)
+    await client.connect()
+
+    const db = client.db('demo')
+    const collection = db.collection('demo')
+
+    const firstName = req.query.FirstName
+    const lastName = req.query.LastName
+    const location = req.query.Location
+
+    const query = {
+      Type: 'Worker',
     }
-  })
-  app.post('/registration', async (req, res) => {
-    try {
-      const client = new MongoClient(mongoURI)
-      await client.connect()
-      
-      const db = client.db('demo')
-      const collection = db.collection('demo')
-      
-      const { username, password, email } = req.body // Use req.body to access POST data
-      
-      // Check if the username already exists in the database
-      const existingUser = await collection.findOne({ username: req.body.username })
-  
-      if (existingUser) {
-        client.close()
-        return res.status(400).json({ error: 'Username already exists' })
-      }
-  
-      // If the username doesn't exist, insert the new user into the database
-      const newUser = {
-        username,
-        password, // Note: You should hash and salt the password before storing it in a production environment.
-        email,
-        Type: 'Worker' // Assuming all registered users are of type 'Worker', you can adjust this as needed.
-      }
-  
-      const result = await collection.insertOne(newUser)
-  
-      client.close()
-      
-      res.status(201).json({ message: 'User registered successfully', insertedId: result.insertedId })
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ error: 'Error registering user' })
+
+    if (req.query.FirstName) {
+      query.FirstName = { $regex: new RegExp(req.query.FirstName, 'i') }
     }
-  })
-  app.post('/login', async (req, res) => {
-    try {
-      const client = new MongoClient(mongoURI)
-      await client.connect()
-  
-      const db = client.db('demo')
-      const collection = db.collection('demo')
-  
-      const { username, password } = req.body // Use req.body to access POST data
-  
-      // Find the user with the provided username
-      const user = await collection.findOne({ username: req.body.username })
-      //const workers = await collection.find({ Type: 'Worker' }).toArray()
-  
-      if (!user) {
-        client.close()
-        return res.status(401).json({ error: 'User not found' })
-      }
-  
-      // For simplicity, we compare the password as plain text. In a real-world app, you should hash and compare it securely.
-      if (user.password !== password) {
-        client.close()
-        return res.status(401).json({ error: 'Invalid password' })
-      }
-  
-      // If the username and password match, you can consider the user authenticated.
-      // You can generate and send a JWT (JSON Web Token) to the client for further authentication.
-  
-      client.close()
-  
-      res.status(200).json({ message: 'Login successful', user })
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ error: 'Error during login' })
+
+    if (req.query.LastName) {
+      query.LastName = { $regex: new RegExp(req.query.LastName, 'i') }
     }
-  })
-  
-  
-  app.listen(port, () => {
-    console.log(`API server is running on port ${port}`)
-  })
+
+    if (req.query.Location) {
+      query.Location = { $regex: new RegExp(req.query.Location, 'i') }
+    }
+
+    const workers = await collection.find(query).toArray()
+
+    client.close()
+    res.json(workers)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'error accessing mongoDB data' })
+  }
+})
+
+app.get('/gigs', async (req, res) => {
+  try {
+    const client = new MongoClient(mongoURI)
+    await client.connect()
+
+    const db = client.db('demo')
+    const collection = db.collection('demo')
+
+    const gigs = await collection.find({ Type: { $ne: 'Worker' } }).toArray()
+
+    client.close()
+    res.json(gigs)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'error accessing mongoDB data' })
+  }
+})
+
+app.get('/get-gigs/', async (req, res) => {
+  try {
+    const client = new MongoClient(mongoURI)
+    await client.connect()
+
+    const db = client.db('demo')
+    const collection = db.collection('demo')
+
+    const title = req.query.title
+    const location = req.query.location
+
+    const query = {
+      Type: { $ne: 'Worker' },
+    }
+
+    if (req.query.title) {
+      query.title = { $regex: new RegExp(req.query.title, 'i') }
+    }
+
+    if (req.query.location) {
+      query.location = { $regex: new RegExp(req.query.location, 'i') }
+    }
+
+    const gigs = await collection.find(query).toArray()
+
+    client.close()
+    res.json(gigs)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'error accessing mongoDB data' })
+  }
+})
+
+app.post('/registration', async (req, res) => {
+  try {
+    const username = req.query.username
+    const password = req.query.password 
+    const email = req.query.email
+
+    if (!username || !password || !email) {
+      return res.status(400).json({ error: 'All fields (Username, Password, Email) are required' })
+    }
+
+    const client = new MongoClient(mongoURI)
+    await client.connect()
+    
+    const db = client.db('demo')
+    const collection = db.collection('demo')
+
+    const existingUser = await collection.findOne({ Username: username })
+
+    if (existingUser) {
+      client.close()
+      return res.status(400).json({ error: 'Username already exists' })
+    }
+
+    const newUser = {
+      Username: username,
+      Password: password,
+      Email: email,
+      Type: 'Worker',
+      isRegistered: true
+    }
+
+    const result = await collection.insertOne(newUser)
+
+    client.close()
+
+    res.status(201).json({ message: 'User registered successfully', newUser })
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Error registering user' })
+  }
+})
+
+app.post('/login', async (req, res) => {
+  try {
+    const client = new MongoClient(mongoURI)
+    await client.connect()
+
+    const db = client.db('demo')
+    const collection = db.collection('demo')
+
+    const username = req.query.username
+    const password = req.query.password
+
+    if (!username || !password) {
+      return res.status(400).json({ error: 'All fields (Username, Password) are required' })
+    }
+
+    const user = await collection.findOne({ Username: username, Password: password })
+
+    if (!user) {
+      client.close()
+      return res.status(401).json({ error: 'Invalid username or password' })
+    }
+
+    client.close()
+
+    res.status(200).json({ message: 'Login successful', user })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Error during login' })
+  }
+})
+
+app.listen(port, () => {
+  console.log(`API server is running on port ${port}`)
+})
