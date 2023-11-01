834d9b9176c1456c4cbebedf206d706d9dff09b6
Sidney <sid.14boys@yahoo.fr>
2023-11-01 03:49:56 -0700

    Job Post Button refined style

--- application/client/src/component/ui/Feed.js
+++ application/client/src/component/ui/Feed.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import bartenderJob from './bartenderJob.png'
-import JobDetails from './JobDetails'
+
 // * LIBRARY IMPORT
 import axios from 'axios'
+
+// * COMPONENT IMPORT
+import bartenderJob from './bartenderJob.png'
+import JobDetails from './JobDetails'
 
 // * CSS IMPORT
 import FeedCSS from './Feed.module.css'
@@ -34,7 +37,7 @@ function Feed () {
   }
 
   // use the slice method to limit the number of job titles to 4
-  const displayedJobTitles = jobTitles.slice(0, 4)
+  // const displayedJobTitles = jobTitles.slice(0, 4)
 
   return (
     <div className={FeedCSS.feed}>
@@ -44,8 +47,8 @@ function Feed () {
             <img src={bartenderJob} alt='Job' width='250' height='150' />
             {job.title}
             <div className={FeedCSS.jobButtonContainer}>
-              <button onClick={() => openJobDetails(job)}>Details</button>
-              <a href='/JobRequested'>Send job request</a>
+              <button onClick={() => openJobDetails(job)} className={FeedCSS.JobButton}>Details</button>
+              <button href='/JobRequested' className={FeedCSS.JobButton}>Send job request</button>
             </div>
           </div>
         </div>
