acfc399333ad46cc001f00a94f19ae2b1a1298ea
AbdarrahmanAyyaz <abdarrahman19@yahoo.com>
2023-11-08 16:49:51 -0800

    feature: setting up dependencies for multer and mongoose along with implementation of schema and upload route. In progress.

--- application/backend/server/server.js
+++ application/backend/server/server.js
@@ -2,10 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
-const multer = require('multer')
-const mongoose = require('mongoose')
-
-const ImageModel = require("/Users/abdarrahmanayyaz/Desktop/648demo/CSC648-demo-team03/application/backend/api/routes/image.model.js")
 const config = {
   baseUrl: '/'
 }
@@ -13,37 +9,6 @@ const config = {
 const indexRouter = express.Router()
 
 const baseDir = path.dirname(path.dirname(__dirname)) // Removes two levels of the path
-
-// STORAGE
-
-const Storage = multer.diskStorage({
-  destination: 'uploads',
-  filename:(req,file,cb)=>{
-    cb(null, file.originalname);
-  },
-});
-const upload = multer({
-  storage:Storage
-}).single('testImage')
-
-app.post('upload',(req,res)=>{
-  upload(req,res,(err)=>{
-    if(err){
-      console.log(err)
-    }
-    else{
-      const newImage = new ImageModel({
-        name: req.body.name,
-        image:{
-          data:req.file.filename,
-          contentType:'image/png'
-        },
-      });
-      newImage.save()
-      .then(()=> res.send("successfully uploaded")).catch(err=> console.log(err)) 
-    }
-  })
-})
 
 // Serve static assets
 app.use('/dist', express.static(path.join(baseDir, 'static/dist')))
