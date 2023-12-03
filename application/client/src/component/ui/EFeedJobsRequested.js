f72a71ecf2f6900d3ade08554c02f016b421f883
Sidney <sid.14boys@yahoo.fr>
2023-12-02 21:01:39 -0800

    Populate data JobD of Employer Job feed done 99%

--- application/client/src/component/ui/EFeedJobsRequested.js
+++ application/client/src/component/ui/EFeedJobsRequested.js
@@ -3,18 +3,12 @@ import React, { useState, useEffect } from 'react'
 // * LIBRARY IMPORT
 import axios from 'axios'
 
-// * COMPONENT IMPORT
+// * COMPONENTS IMPORT
 import bartenderJob from './bartenderJob.png'
 import JobDetails from './JobDetails'
 
 // * CSS IMPORT
 import EFeedJobsRequestedCSS from './EFeedJobsRequested.module.css'
-
-// TODO: Remove displayedJobTitles and replace with jobTitles corresponding to the employer
-// TODO: Refined pop-up for job details
-// TODO: pop-up remove static data and populate with data from the database
-// TODO: Online button should be reflecting the status of the posted job (online/offline)
-// TODO: get Employer/Settings buttons to work
 
 function EFeedJobsRequested () {
   const [jobTitles, setJobTitles] = useState([])
@@ -25,50 +19,57 @@ function EFeedJobsRequested () {
 
     axios.get(apiUrl)
       .then(response => {
-        // extract job titles from the response and update the state
-        const titles = response.data.map(job => job.title)
-        setJobTitles(titles)
+        const jobs = response.data // store full job data
+        console.log('Fetched jobs:', jobs)
+        setJobTitles(jobs) // update state with full job data
       })
       .catch(error => {
         console.error('Error fetching data:', error)
       })
   }, [])
 
-  const openJobDetails = (job) => {
-    setSelectedJob(job)
+  const openJobDetails = (selectedTitle) => {
+    // find the job object based on the selected title
+    const jobDetails = jobTitles.find(job => job.title === selectedTitle)
+    if (jobDetails) {
+      console.log('Selected job:', jobDetails)
+      setSelectedJob(jobDetails)
+    }
   }
 
   const closeJobDetails = () => {
     setSelectedJob(null)
   }
 
-  // * Currently displaying 2 jobs onn purpose to match UI design
-  // use the slice method to limit the number of job titles to 4
   const displayedJobTitles = jobTitles.slice(0, 2)
 
   return (
     <div>
-    <h1 className={EFeedJobsRequestedCSS.Title}>JOBS</h1>
-    <div className={EFeedJobsRequestedCSS.feed}>
-      {displayedJobTitles.map((job, index) => (
-        <div key={index} className={`${EFeedJobsRequestedCSS.post}`}>
-          <div className={`${EFeedJobsRequestedCSS.item} item-${index + 1}`}>
-            <img src={bartenderJob} alt='Job' width='350' height='170' />
-            <div className={EFeedJobsRequestedCSS.JobTitle}>{job}</div>
-            <div className={EFeedJobsRequestedCSS.jobButtonContainer}>
-              <button onClick={() => openJobDetails(job)} className={EFeedJobsRequestedCSS.JobButton}>Details</button>
-              <button href='/JobRequested' className={EFeedJobsRequestedCSS.JobButton}>Online</button>
+      <h1 className={EFeedJobsRequestedCSS.Title}>JOBS</h1>
+      <div className={EFeedJobsRequestedCSS.feed}>
+        {displayedJobTitles.map((job, index) => (
+          <div key={index} className={`${EFeedJobsRequestedCSS.post}`}>
+            <div className={`${EFeedJobsRequestedCSS.item} item-${index + 1}`}>
+              <img src={bartenderJob} alt='Job' width='350' height='170' />
+              <div className={EFeedJobsRequestedCSS.JobTitle}>{job.title}</div>
+              <div className={EFeedJobsRequestedCSS.jobButtonContainer}>
+                <button onClick={() => openJobDetails(job.title)} className={EFeedJobsRequestedCSS.JobButton}>
+                  Details
+                </button>
+                <button href='/JobRequested' className={EFeedJobsRequestedCSS.JobButton}>
+                  Online
+                </button>
+              </div>
             </div>
           </div>
-
-        </div>
-      ))}
+        ))}
         <button className={EFeedJobsRequestedCSS.AddJobButton}>Add a job</button>
-      {selectedJob && (
-        <JobDetails jobDetails={selectedJob} onClose={closeJobDetails} />
-      )}
-    </div>
+        {selectedJob && (
+          <JobDetails jobDetails={selectedJob} onClose={closeJobDetails} />
+        )}
+      </div>
     </div>
   )
 }
+
 export default EFeedJobsRequested
