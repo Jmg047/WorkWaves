70df5ba7f435569fab432daf65702ad6a2804b63
Sidney <sid.14boys@yahoo.fr>
2023-12-03 15:25:05 -0800

    Send Job Request pop up done

--- application/client/src/component/ui/SendJobRequest.js
+++ application/client/src/component/ui/SendJobRequest.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import SendJobRequestCSS from './SendJobRequest.module.css'
 
+//TODO: refines send job request pop-up style
 function SendJobRequest ({ jobTitle, onClose }) {
   return (
     <div className={SendJobRequestCSS.popup}>
       <div className={SendJobRequestCSS.popupContent}>
-      <h2>Send a request for</h2>
+      <h1>Send a request for</h1>
 
       <h2 className={SendJobRequestCSS.title}><strong>Title: </strong> {jobTitle}</h2>
 
-      <p>The owner of this job post will receive your profile info whether they
-        accept or not your request. make sure you info are up to date.
-      </p>
+      <div className={SendJobRequestCSS.infos}> 
+        <p>The owner of this job post will receive your profile info whether they
+           accept or not your request. Make sure you info are up to date.
+        </p>
+      </div>
+     
 
           <button
           className={SendJobRequestCSS.closeButton}>
