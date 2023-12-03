caef765b8a998306a6ecad1c2cd2ee8fc0e8e977
Sidney <sid.14boys@yahoo.fr>
2023-12-03 06:11:29 -0800

    More Progress SendJobRequest

--- application/client/src/component/ui/SendJobRequest.js
+++ application/client/src/component/ui/SendJobRequest.js
@@ -6,7 +6,24 @@ function SendJobRequest ({  onClose }) {
   return (
     <div className={SendJobRequestCSS.popup}>
       <div className={SendJobRequestCSS.popupContent}>
-      <p>Send job request here</p>
+      <h2>Send a request for</h2>
+
+      <h2 className={JobDetailsCSS.title}><strong>Title: </strong>{jobDetails.title}</h2>
+
+      <p>The owner of this job post will receive your profile info whether they 
+        accept or not your request. make sure you info are up to date.
+      </p>
+
+          <button 
+          className={SendJobRequestCSS.closeButton}>
+          Check my profile
+          </button>
+
+          <button 
+          className={SendJobRequestCSS.closeButton}>
+          Send a request
+          </button>
+
         <button 
           onClick={onClose} 
           className={SendJobRequestCSS.closeButton}>
