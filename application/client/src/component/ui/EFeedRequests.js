483fb1f97e2f6bbd8322928c800b399abb60a38a
Sidney <sid.14boys@yahoo.fr>
2023-12-04 03:48:17 -0800

    Details Pop up added + Data populated

--- application/client/src/component/ui/EFeedRequests.js
+++ application/client/src/component/ui/EFeedRequests.js
@@ -1,42 +1,77 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ElonMusk from './ElonMusk.png';
+import JobDetails from './JobDetails';
+import EFeedRequestsCSS from './EFeedRequests.module.css';
 
-// * LIBRARY IMPORT
+function EFeedRequests({ selectedCategory }) {
+  const [jobTitles, setJobTitles] = useState([]);
+  const [selectedJob, setSelectedJob] = useState(null);
 
-// * COMPONENT IMPORT
-import ElonMusk from './ElonMusk.png'
+  useEffect(() => {
+    let apiUrl = 'https://workwaves-jm2b5.ondigitalocean.app/api/get-gigs';
 
-// * CSS IMPORT
-import EFeedRequestsCSS from './EFeedRequests.module.css'
+    if (selectedCategory) {
+      apiUrl = `https://workwaves-jm2b5.ondigitalocean.app/api/get-gigs?category=${selectedCategory}`;
+    }
+    
+    axios.get(apiUrl)
+      .then(response => {
+        const titles = response.data.map(job => job.title);
+        setJobTitles(titles.slice(0, 2)); // display only the first two job titles
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, [selectedCategory]);
 
-// TODO: populate right data from DB
-// TODO: pop up button details here (Banting)
-// TODO: make accept and decline buttons works
+  const openJobDetails = (jobTitle) => {
+    axios.get(`https://workwaves-jm2b5.ondigitalocean.app/api/get-gigs?title=${jobTitle}`)
+      .then(response => {
+        if (response.data.length > 0) {
+          const jobDetails = response.data[0];
+          setSelectedJob(jobDetails);
+        } else {
+          console.log('No job details found for this title.');
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching job details:', error);
+      });
+  };
 
-// testsc s
+  const closeJobDetails = () => {
+    setSelectedJob(null);
+  };
 
-function EFeedRequests () {
   return (
-      <div>
+    <div>
       <h1 className={EFeedRequestsCSS.Title}>Requests</h1>
       <div className={EFeedRequestsCSS.Feed}>
-        <div className={EFeedRequestsCSS.RequestsContainer}>
-          <div className={EFeedRequestsCSS.RequestsPhotos}>
-            <img className= {EFeedRequestsCSS.Photos}src={ElonMusk} alt='Job' width='280' height='220' />
+        {jobTitles.map((jobTitle, index) => (
+          <div key={index} className={EFeedRequestsCSS.RequestsContainer}>
+            <div className={EFeedRequestsCSS.RequestsPhotos}>
+              <img className={EFeedRequestsCSS.Photos} src={ElonMusk} alt='Job' width='280' height='220' />
             </div>
-          <div className={EFeedRequestsCSS.RequestsDetails}>
-            <div className={EFeedRequestsCSS.IsRequestFor}> is requesting for </div>
-            <div> Looking for a bartender </div>
-            <button>Details</button>
+            <div className={EFeedRequestsCSS.RequestsDetails}>
+              <div className={EFeedRequestsCSS.IsRequestFor}>
+                is requesting for
+              </div>
+              <div>{`Looking for: ${jobTitle}`}</div>
+              <button onClick={() => openJobDetails(jobTitle)}>Details</button>
             </div>
-          <div className={EFeedRequestsCSS.RequestsButtons}>
-
-          <button>Accept</button>
-          <button>Decline</button>
+            <div className={EFeedRequestsCSS.RequestsButtons}>
+              <button>Accept</button>
+              <button>Decline</button>
             </div>
-        </div>
+          </div>
+        ))}
       </div>
-      </div>
-  )
+      {selectedJob && (
+        <JobDetails jobDetails={selectedJob} onClose={closeJobDetails} />
+      )}
+    </div>
+  );
 }
 
-export default EFeedRequests
+export default EFeedRequests;
--- application/client/src/component/ui/Feed.js
+++ application/client/src/component/ui/Feed.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-// import JobRequest from './JobRequest'
 
 // * LIBRARY IMPORT
 import axios from 'axios'
